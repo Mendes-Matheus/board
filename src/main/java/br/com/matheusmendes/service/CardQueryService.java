4c3d02daee281c08376c209b43aff63144361ab4
Matheus Mendes <msmmendes211@gmail.com>
2025-04-10 19:10:59 -0300

    feat: implementa interfaces de usuário: BoardMenu e MainMenu para interação com o usuário na gestão de boards e cards

---
+++ src/main/java/br/com/matheusmendes/service/CardQueryService.java
@@ -0,0 +1,21 @@
+package br.com.matheusmendes.service;
+
+import br.com.matheusmendes.dto.CardDetailsDTO;
+import br.com.matheusmendes.persistence.dao.CardDAO;
+import lombok.AllArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class CardQueryService {
+
+    private final Connection connection;
+
+    public Optional<CardDetailsDTO> findById(final Long id) throws SQLException {
+        var dao = new CardDAO(connection);
+        return dao.findById(id);
+    }
+
+}
